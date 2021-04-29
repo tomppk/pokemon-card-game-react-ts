75efcc4e0bea03d0fec55e21f2f589b70865c25f
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-29 16:06:30 +0300

    add state player, difficulty, deckart to StartMenu component

--- src/components/StartMenu.tsx
+++ src/components/StartMenu.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const StartMenu = () => {
+  // Component level state with default values
+  const [playerName, setPlayerName] = useState('');
+  const [difficulty, setDifficulty] = useState('easy');
+  const [deckArt, setDeckArt] = useState('standard');
+
   // useEffect invokes callback when StartMenu component is rendered first
   // time
   useEffect(() => {
+    // Prevent submitting of start menu form
+    const startMenuForm = document.getElementById('startMenuForm');
+    if (startMenuForm)
+      startMenuForm.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+      });
+
     // Selector for start menu form label
     const labels = document.querySelectorAll('.form-control label');
 
@@ -21,19 +32,35 @@ const StartMenu = () => {
     });
   }, []);
 
+  // Function to initialize new game
+  const initializeGame = () => {
+    console.log(playerName);
+    console.log(difficulty);
+    console.log(deckArt);
+  };
+
+  // JSX to render
   return (
     <div id="startMenuContainer">
       <h1>Pokemon Card Game</h1>
       <form id="startMenuForm" autoComplete="off">
         <div className="form-control">
-          <input type="text" name="playerName" required />
+          <input
+            value={playerName}
+            onChange={(e) => setPlayerName(e.target.value)}
+            type="text"
+            required
+          />
           <label>Enter Your Name</label>
         </div>
 
         <div id="selectElement">
           <label>Select Difficulty: </label>
 
-          <select name="difficulty" id="difficulty">
+          <select
+            value={difficulty}
+            onChange={(e) => setDifficulty(e.target.value)}
+            id="difficulty">
             <option value="easy">Easy</option>
             <option value="medium">Medium</option>
             <option value="hard">Hard</option>
@@ -43,13 +70,16 @@ const StartMenu = () => {
         <div id="selectDeck">
           <label>Select Deck:</label>
 
-          <select name="deck" id="deck">
+          <select
+            value={deckArt}
+            onChange={(e) => setDeckArt(e.target.value)}
+            id="deck">
             <option value="standard">Standard</option>
             <option value="dream-world">Dream World</option>
             <option value="official-artwork">Official Artwork</option>
           </select>
         </div>
-        <button className="btn" id="newGameButton">
+        <button onClick={initializeGame} className="btn" id="newGameButton">
           Start a New Game
         </button>
       </form>
